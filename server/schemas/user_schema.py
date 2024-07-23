e12056fbda99cda256a7f8c3863461fda424835d
Jonghun Shin <ozingmw@naver.com>
2024-07-23 16:23:36 +0900

    update, delete user

--- server/schemas/user_schema.py
+++ server/schemas/user_schema.py
@@ -3,6 +3,7 @@ from typing import Literal, Optional
 
 from models.user_model import User
 
+# output은 User로 해도 되는데 input은 User로 하면 안됨, 다 써야함
 
 class CreateUserInput(BaseModel):
     nickname: str
@@ -11,12 +12,21 @@ class CreateUserInput(BaseModel):
 
 class CreateUserOutput(BaseModel):
     user: Optional[User]
-
-class ReadUserInput(BaseModel):
-    user_id: int
-
+    
 class ReadUserOutput(BaseModel):
+    user: Optional[User]
+
+class UpdateUserInput(BaseModel):
     user_id: int
     nickname: str
     sex: Literal['M', 'F']
     age: int
+
+class UpdateUserOutput(BaseModel):
+    user: Optional[User]
+
+class DeleteUserInput(BaseModel):
+    user_id: int
+
+class DeleteUserOutput(BaseModel):
+    bool
--- server/views/user_view.py
+++ server/views/user_view.py
@@ -19,20 +19,59 @@ def create_user(create_user_input: CreateUserInput, db: Session) -> User:
         db.refresh(user)
 
         return user
+    
     except IntegrityError as e:
         db.rollback()
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="이미 존재하는 사용자입니다")
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 사용자입니다")
 
-def get_user(user_id: int, db: Session):
-    user = db.query(UserTable).filter(UserTable.user_id == user_id).first()
+def read_user(db: Session):
+    user = db.query(UserTable).all()
 
-    if user:
-        return {
-            "status_code": status.HTTP_200_OK,
-            "detail": "사용자 조회 성공",
-            "data": user
-        }
-    else:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail="일치하는 사용자가 존재하지 않습니다")
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 사용자가 존재하지 않습니다")
+    
+    return user
+
+# def read_user_by_user_id(user_id: int, db: Session) -> User:
+#     user = db.query(UserTable).filter(UserTable.user_id == user_id).first()
+
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 사용자가 존재하지 않습니다")
+    
+#     return user
+    
+def update_user(update_user_input: UpdateUserInput, db: Session) -> User:
+    try:
+        user = db.query(UserTable).filter(UserTable.user_id == update_user_input.user_id).first()
+
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 사용자가 존재하지 않습니다")
+        
+        for key, value in update_user_input.model_dump().items():
+            setattr(user, key, value)
+
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+        return user
+
+    except IntegrityError as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="수정 실패")
+
+def delete_user(delete_user_input: DeleteUserInput, db: Session) -> bool:
+    try:
+        user = db.query(UserTable).filter(UserTable.user_id == delete_user_input.user_id).first()
+
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 사용자가 존재하지 않습니다")
+        
+        db.delete(user)
+        db.commit()
+
+        return True
+
+    except IntegrityError as e:
+        db.rollback()
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="삭제 실패")
