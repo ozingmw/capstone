b95a25bfa2671dab9fc8f5004134949f43cf168e
Ccolornyong <ccolornyong@gmail.com>
2024-08-25 10:46:00 +0900

    12

---
+++ client/lib/diary1.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'widgets/bottomNavi.dart';
+import 'package:client/gin3.dart';
+import './gin2.dart';
+import './main2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/gin2': (context) => const gin2(),
+        '/gin3': (context) => const gin3(),
+        '/main2': (context) => const main2(),
+      },
+      home: const diary1(),
+    );
+  }
+}
+
+class diary1 extends StatefulWidget {
+  const diary1({super.key});
+
+  @override
+  State<diary1> createState() => _diary1State();
+}
+
+class _diary1State extends State<diary1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Spacer(), // 남은 공간을 차지하여 버튼을 오른쪽으로 밀어냄
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('저장'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                    '10',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      decorationColor: Color.fromARGB(255, 0, 0, 0),
+                      decorationThickness: 2,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    ),
+                  ),
+                ),
+                Text(
+                  '화요일',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              '올해 꼭 이루고 싶은 소원 세가지는 무엇인가요?',
+              style: TextStyle(
+                fontSize: 16,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              width: 450,
+              height: 400,
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 145, 171, 145),
+                borderRadius: BorderRadius.circular(40),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: const bottomNavi(),
+    );
+  }
+}
--- client/lib/test.dart
+++ client/lib/test.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:table_calendar/table_calendar.dart';
+import 'widgets/bottomNavi.dart';
+import 'package:client/gin3.dart';
+import './gin2.dart';
+import './main2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,132 +13,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
+    return MaterialApp(
+      routes: {
+        '/gin2': (context) => const gin2(),
+        '/gin3': (context) => const gin3(),
+        '/main2': (context) => const main2(),
+      },
+      home: const diary1(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class diary1 extends StatefulWidget {
+  const diary1({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<diary1> createState() => _diary1State();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  DateTime _focusedDay = DateTime.now();
-  DateTime? _selectedDay;
-  CalendarFormat _calendarFormat = CalendarFormat.month;
-  final Map<DateTime, List<String>> _events = {
-    DateTime.utc(2024, 8, 18): ['Test Event 1'],
-    DateTime.utc(2024, 8, 20): ['Test Event 2'],
-  };
-
-  // Listenable to hold selected events
-  final ValueNotifier<List<String>> _selectedEvents = ValueNotifier([]);
-
+class _diary1State extends State<diary1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Calendar with Events')),
-      body: Column(
-        children: <Widget>[
-          TableCalendar(
-            firstDay: DateTime.utc(2022, 10, 16),
-            lastDay: DateTime.utc(2027, 3, 14),
-            focusedDay: _focusedDay,
-            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-            calendarFormat: _calendarFormat,
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-                _calendarFormat = CalendarFormat.week;
-                _updateSelectedEvents(selectedDay);
-              });
-            },
-            onPageChanged: (focusedDay) {
-              setState(() {
-                _focusedDay = focusedDay;
-              });
-            },
-            onFormatChanged: (format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            },
-            eventLoader: (day) => _events[day] ?? [],
-            calendarBuilders: CalendarBuilders(
-              markerBuilder: (context, date, events) {
-                if (events.isNotEmpty) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: events.map((event) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 1.0),
-                        child: Container(
-                          width: 7.0,
-                          height: 7.0,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.red,
-                          ),
-                        ),
-                      );
-                    }).toList(),
-                  );
-                }
-                return const SizedBox();
-              },
-            ),
-          ),
-          Expanded(
-            child: ValueListenableBuilder<List<String>>(
-              valueListenable: _selectedEvents,
-              builder: (context, value, _) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 4.0,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.circular(12.0),
-                      ),
-                      child: ListTile(
-                        onTap: () => print(value[index]),
-                        title: Text(value[index]),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  // Update the selected events based on the selected day
-  void _updateSelectedEvents(DateTime? selectedDay) {
-    if (selectedDay != null) {
-      final events = _events[selectedDay] ?? [];
-      _selectedEvents.value = events;
-    } else {
-      _selectedEvents.value = [];
-    }
-  }
-
-  // Check if two dates are the same day
-  bool isSameDay(DateTime? day1, DateTime day2) {
-    if (day1 == null) return false;
-    return day1.year == day2.year &&
-        day1.month == day2.month &&
-        day1.day == day2.day;
+    return const Scaffold();
   }
 }
