c1de3753bc52da681de849539f273f93df468fec
Jonghun Shin <ozingmw@naver.com>
2024-09-02 22:37:40 +0900

    ios & android google login

--- server/routers/login_router.py
+++ server/routers/login_router.py
@@ -8,7 +8,7 @@ from google.auth.transport import requests
 from core.config import Settings
 from db.connection import get_db
 
-from auth import auth_handler, google
+from auth import auth_handler
 from apis import user
 from schemas.user_schema import *
 from schemas.login_schema import *
@@ -16,14 +16,6 @@ from schemas.login_schema import *
 
 router = APIRouter(prefix="/login", tags=["LOGIN"])
 settings = Settings()
-
-
-# @router.post("/")
-# def login(login_input: LoginInput, db: Session = Depends(get_db)) -> User:
-#     hashed_token = auth_handler.verify_access_token(login_input.encoded_token)
-#     res = user.read_user_token(hashed_token=hashed_token['id'], db=db)
-
-#     return JSONResponse(status_code=status.HTTP_200_OK, content={"res": jsonable_encoder(res)})
 
 
 @router.post("/register")
@@ -39,27 +31,7 @@ async def register(register_input: RegisterInput, db: Session = Depends(get_db))
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"res": jsonable_encoder(res)})
 
 
-# @router.get('/auth')
-# def auth_google():
-#     auth_url = google.auth()
-#     return RedirectResponse(auth_url)
-
-# @router.get('/google')
-# def auth_google_callback(code: Optional[str] = None, error: Optional[str] = None, db: Session = Depends(get_db)):
-#     if code:
-#         token_data = google.auth_callback(code=code)
-
-#         user_exist = user.read_user_email(token_data['email'], db=db)
-
-#         jwt_token = auth_handler.create_access_token(token_data)
-
-#         return {"access_token": jwt_token, "token_type": "bearer", "user_exist": user_exist}
-    
-#     elif error:
-#         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": "로그인 실패!", "error": error})
-
-
-@router.post('/google/ios')
+@router.post('/google')
 async def google_auth_test(request: LoginInput, db: Session = Depends(get_db)):
     try:
         user_data = id_token.verify_oauth2_token(request.token, requests.Request(), settings.IOS_GOOGLE_CLIENT_ID)
