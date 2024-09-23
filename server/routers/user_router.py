8e4c5d04b6ed3c35c9381a1a4e52b19fbdf7f514
Jonghun Shin <ozingmw@naver.com>
2024-09-23 13:40:46 +0900

    add read user

--- server/routers/user_router.py
+++ server/routers/user_router.py
@@ -21,6 +21,13 @@ def create_user(create_user_input: CreateUserInput, db: Session = Depends(get_db
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"res": jsonable_encoder(res)})
 
 
+@router.get("/read")
+def read_user(db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BaseUserOutput:
+    res = user.check_token(db=db, token=token)
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"res": jsonable_encoder(res)})
+
+
 @router.post("/update/nickname")
 def update_user_nickname(update_user_nickname_input: UpdateUserNicknameInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BaseUserOutput:
     res = user.update_user_nickname(update_user_nickname_input=update_user_nickname_input, db=db, token=token)
