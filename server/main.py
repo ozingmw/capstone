02c58c1cd70966c374ebc724e1b299e4668092d3
Jonghun Shin <ozingmw@naver.com>
2024-10-13 22:07:42 +0900

    add server logger

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import Depends, FastAPI
 from db.session import engine, Base
+from logging.config import dictConfig
 
 from routers import db_check
 from routers.user_router import router as user_router
@@ -25,7 +26,10 @@ app.include_router(sentiment_router, dependencies=[Depends(JWTBearer())])
 app.include_router(quote_router, dependencies=[Depends(JWTBearer())])
 app.include_router(diary_router, dependencies=[Depends(JWTBearer())])
 
+from routers.sentiment import router as test_router
+app.include_router(test_router)
+from core import log
+dictConfig(log.logger)
 
 if __name__ == "__main__":
-    uvicorn.run('main:app', host='0.0.0.0', port=9977, reload=True) # 외부 접속 가능
-    # uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=9977, reload=True)
