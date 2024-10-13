f90fe333b74c9ce3a4d4b8ebca0655f5b903a877
Jonghun Shin <ozingmw@naver.com>
2024-10-13 22:08:25 +0900

    update diary

--- server/apis/diary.py
+++ server/apis/diary.py
@@ -11,8 +11,8 @@ from models.user_model import UserTable
 
 def write_diary(write_diary_input: WriteDiaryInput, db: Session, token: str) -> Diary:
     try:
-        decode_token = auth_handler.verify_access_token(token)
-        user = db.query(UserTable).filter(UserTable.hashed_token == decode_token['id']).first()
+        decode_token = auth_handler.verify_access_token(token)['id']
+        user = db.query(UserTable).filter(UserTable.hashed_token == decode_token).first()
         
         diary = DiaryTable(
             user_id=user.user_id,
@@ -32,20 +32,37 @@ def write_diary(write_diary_input: WriteDiaryInput, db: Session, token: str) -> 
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 diary입니다")
 
 
-def read_monthly_diary(read_monthly_diary_input: ReadMonthlyDiaryInput, db: Session) -> Diary:
+def read_monthly_diary(read_monthly_diary_input: ReadMonthlyDiaryInput, db: Session, token: str) -> Diary:
+    decode_token = auth_handler.verify_access_token(token)['id']
+    user = db.query(UserTable).filter(UserTable.hashed_token == decode_token).first()
+
     diary = db.query(DiaryTable).join(UserTable).filter(
-        UserTable.hashed_token == read_monthly_diary_input.token['id'],
-        DiaryTable.user_id == UserTable.user_id,
-        extract('month', DiaryTable.daytime) == read_monthly_diary_input.date.month,
-        extract('year', DiaryTable.daytime) == read_monthly_diary_input.date.year
+        UserTable.hashed_token == decode_token,
+        extract('year', DiaryTable.daytime) == read_monthly_diary_input.date.year,
+        extract('month', DiaryTable.daytime) == read_monthly_diary_input.date.month
     ).all()
 
     return diary
 
 
-def read_diary(read_diary_input: ReadDiaryInput, db: Session) -> Diary:
+def read_weekly_diary(read_weekly_diary_input: ReadWeeklyDiaryInput, db: Session, token: str) -> Diary:
+    decode_token = auth_handler.verify_access_token(token)['id']
+    user = db.query(UserTable).filter(UserTable.hashed_token == decode_token).first()
+
     diary = db.query(DiaryTable).join(UserTable).filter(
-        UserTable.hashed_token == read_diary_input.token['id'],
+        UserTable.hashed_token == decode_token,
+        extract('year', DiaryTable.daytime) == read_weekly_diary_input.date.year,
+        extract('month', DiaryTable.daytime) == read_weekly_diary_input.date.month
+    ).all()
+
+    return diary
+
+
+def read_diary(read_diary_input: ReadDiaryInput, db: Session, token: str) -> Diary:
+    decode_token = auth_handler.verify_access_token(token)['id']
+
+    diary = db.query(DiaryTable).join(UserTable).filter(
+        UserTable.hashed_token == decode_token,
         DiaryTable.daytime == read_diary_input.date
     ).all()
 
@@ -71,66 +88,6 @@ def modify_diary(modify_diary_input: ModifyDiaryInput, db: Session) -> Diary:
     return diary
 
 
-# ------ 사용 X ------
-
-
-# def create_diary(create_diary_input: CreateDiaryInput, db: Session) -> Diary:
-#     diary = DiaryTable(diary_content=create_diary_input.diary_content)
-
-#     try:
-#         db.add(diary)
-#         db.commit()
-#         db.refresh(diary)
-
-#         return diary
-    
-#     except IntegrityError as e:
-#         db.rollback()
-#         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="이미 존재하는 diary입니다")
-
-
-# def read_diary(db: Session):
-#     diary = db.query(DiaryTable).all()
-
-#     if not diary:
-#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 diary이 존재하지 않습니다")
-    
-#     return diary
-    
-
-# def update_diary(update_diary_input: UpdateDiaryInput, db: Session) -> Diary:
-#     try:
-#         diary = db.query(DiaryTable).filter(DiaryTable.diary_id == update_diary_input.diary_id).first()
-
-#         if not diary:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 diary이 존재하지 않습니다")
-        
-#         for key, value in update_diary_input.model_dump().items():
-#             setattr(diary, key, value)
-
-#         db.add(diary)
-#         db.commit()
-#         db.refresh(diary)
-
-#         return diary
-
-#     except IntegrityError as e:
-#         db.rollback()
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="수정 실패")
-
-
-# def delete_diary(delete_diary_input: DeleteDiaryInput, db: Session) -> bool:
-#     try:
-#         diary = db.query(DiaryTable).filter(DiaryTable.diary_id == delete_diary_input.diary_id).first()
-
-#         if not diary:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="일치하는 diary이 존재하지 않습니다")
-        
-#         db.delete(diary)
-#         db.commit()
-
-#         return True
-
-#     except IntegrityError as e:
-#         db.rollback()
-#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="삭제 실패")
+def analyze_diary(analyze_diary_input: AnalyzeDiaryInput, db: Session) -> AnalyzeDiaryOutput:
+    # 모델 불러와서 분석해주는 코드
+    return 0
--- server/routers/diary_router.py
+++ server/routers/diary_router.py
@@ -8,38 +8,48 @@ from db.connection import get_db
 
 from apis import diary
 from schemas.diary_schema import *
-from auth import auth_handler
 
 
 router = APIRouter(prefix="/diary", tags=["DIARY"])
 
 
 @router.post("/write")
-def write_diary(write_diary_input: WriteDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> WriteDiaryOutput:
+def write_diary(write_diary_input: WriteDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BasicDiaryOutput:
     res = diary.write_diary(write_diary_input=write_diary_input, db=db, token=token)
     
     return JSONResponse(status_code=status.HTTP_200_OK, content={'res': jsonable_encoder(res)})
 
 
 @router.post("/read/monthly")
-def read_monthly_diary(read_monthly_diary_input: ReadMonthlyDiaryInput, db: Session = Depends(get_db)) -> ReadMonthlyDiaryOutput:
-    read_monthly_diary_input.token = auth_handler.verify_access_token(read_monthly_diary_input.token)
-    res = diary.read_monthly_diary(read_monthly_diary_input=read_monthly_diary_input, db=db)
+def read_monthly_diary(read_monthly_diary_input: ReadMonthlyDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BasicDiaryOutput:
+    res = diary.read_monthly_diary(read_monthly_diary_input=read_monthly_diary_input, db=db, token=token)
+    
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'res': jsonable_encoder(res)})
+
+
+@router.post("/read/weekly")
+def read_monthly_diary(read_weekly_diary_input: ReadWeeklyDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BasicDiaryOutput:
+    res = diary.read_monthly_diary(read_weekly_diary_input=read_weekly_diary_input, db=db, token=token)
     
     return JSONResponse(status_code=status.HTTP_200_OK, content={'res': jsonable_encoder(res)})
 
 
 @router.post("/read")
-def read_diary(read_diary_input: ReadDiaryInput, db: Session = Depends(get_db)) -> ReadDiaryOutput:
-    read_diary_input.token = auth_handler.verify_access_token(read_diary_input.token)
-    res = diary.read_diary(read_diary_input=read_diary_input, db=db)
+def read_diary(read_diary_input: ReadDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BasicDiaryOutput:
+    res = diary.read_diary(read_diary_input=read_diary_input, db=db, token=token)
     
     return JSONResponse(status_code=status.HTTP_200_OK, content={'res': jsonable_encoder(res)})
 
 
 @router.put("/modify")
-def modify_diary(modify_diary_input: ModifyDiaryInput, db: Session = Depends(get_db)) -> ModifyDiaryOutput:
-    modify_diary_input.token = auth_handler.verify_access_token(modify_diary_input.token)
-    res = diary.modify_diary(modify_diary_input=modify_diary_input, db=db)
+def modify_diary(modify_diary_input: ModifyDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> BasicDiaryOutput:
+    res = diary.modify_diary(modify_diary_input=modify_diary_input, db=db, token=token)
+    
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'r es': jsonable_encoder(res)})
+
+
+@router.post("/analyze")
+def analyze_diary(analyze_diary_input: AnalyzeDiaryInput, db: Session = Depends(get_db), token: str = Depends(JWTBearer())) -> AnalyzeDiaryOutput:
+    res = diary.analyze_diary(analyze_diary_input=analyze_diary_input, db=db, token=token)
     
     return JSONResponse(status_code=status.HTTP_200_OK, content={'res': jsonable_encoder(res)})
