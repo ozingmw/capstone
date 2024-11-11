74fc25954d26c3e7573d569edaa003ff1d704239
ozingmw <ozingmw@naver.com>
2024-11-11 15:31:29 +0900

    update backend

---
+++ server/start.bat
@@ -0,0 +1,6 @@
+@echo off
+CALL "C:\Users\dayclover_admin\miniconda3\condabin\conda.bat" activate capstone
+
+cd "C:\Users\dayclover_admin\Desktop\server"
+
+python "C:\Users\dayclover_admin\Desktop\server\main.py"
